d3c44b59dbc2df5be260a9e6ef2eddba486e9b03
HieuhSSS <hieuhs@widogame.com>
2022-12-07 21:40:33 +0700

    Fix footer layout

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 try {
-    console.log(process.env.MONGO_URL);
     mongoose.connect(process.env.MONGO_URL);
 } catch (err) {
     console.log(err);
