bfe492f6ea5d009e70231f178a727c30355ae503
HieuhSSS <hieuhs@widogame.com>
2022-12-07 12:28:10 +0700

    Add log for mongo env

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 try {
+    console.log(process.env.MONGO_URL);
     mongoose.connect(process.env.MONGO_URL);
 } catch (err) {
     console.log(err);
